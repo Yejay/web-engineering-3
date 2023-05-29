4d62aace75980f48626154b9bf1302d232a21da5
Yejay <demirkan.yejay@gmail.com>
2023-05-29 18:16:37 +0200

    changed port back to 3000, 3006 didnt work

--- frontend/playwright.config.ts
+++ frontend/playwright.config.ts
@@ -51,7 +51,7 @@ const config: PlaywrightTestConfig = defineConfig({
 	/* Run your local dev server before starting the tests */
 	webServer: {
 		command: 'npm run start',
-		url: 'http://127.0.0.1:3006',
+		url: 'http://127.0.0.1:3000',
 		reuseExistingServer: !process.env.CI,
 		timeout: 120 * 1000,
 	},
