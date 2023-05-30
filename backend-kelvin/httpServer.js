1d97c59b1a29dd3e71244cd68230a9c52b660612
Yejay <demirkan.yejay@gmail.com>
2023-05-30 17:04:43 +0200

    changed ports

--- backend-kelvin/httpServer.js
+++ backend-kelvin/httpServer.js
@@ -5,89 +5,91 @@
  */
 
 // NPM Modulimports
-const express = require("express");
-const bodyParser = require("body-parser")
-const fs = require("fs")
-const https = require("https")
-const cors = require("cors")
+const express = require('express');
+const bodyParser = require('body-parser');
+const fs = require('fs');
+const https = require('https');
+const cors = require('cors');
 
 // Datenbankmodul
-const database = require("./database/db")
+const database = require('./database/db');
 // Routenmodule
-const testRoutes = require("./endpoints/test/TestRoutes")
-const publicUserRoutes = require("./endpoints/users/PublicUserRoute")
-const UserService = require("./endpoints/users/UserServices")
-const userRoutes = require("./endpoints/users/UserRoutes")
-const degreeCourseRoutes = require("./endpoints/degreeCourse/degreeCourseRoutes")
-const authenticateRoute = require("./endpoints/authenticate/authenticateRoutes")
-const degreeCourseApplicationRoutes = require("./endpoints/degreeCourseApplication/DegreeCourseApplicationRoute")
+const testRoutes = require('./endpoints/test/TestRoutes');
+const publicUserRoutes = require('./endpoints/users/PublicUserRoute');
+const UserService = require('./endpoints/users/UserServices');
+const userRoutes = require('./endpoints/users/UserRoutes');
+const degreeCourseRoutes = require('./endpoints/degreeCourse/degreeCourseRoutes');
+const authenticateRoute = require('./endpoints/authenticate/authenticateRoutes');
+const degreeCourseApplicationRoutes = require('./endpoints/degreeCourseApplication/DegreeCourseApplicationRoute');
 
 // Middleware
-const key = fs.readFileSync('./certificates/key.pem')
-const cert = fs.readFileSync('./certificates/cert.pem')
+const key = fs.readFileSync('./certificates/key.pem');
+const cert = fs.readFileSync('./certificates/cert.pem');
 const app = express();
-app.use("*", cors())
+app.use('*', cors());
 app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    res.header("Access-Control-Expose-Headers", "Authorization");
-    next();
-})
-app.use(bodyParser.json())
+	res.header('Access-Control-Allow-Origin', '*');
+	res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+	res.header('Access-Control-Expose-Headers', 'Authorization');
+	next();
+});
+app.use(bodyParser.json());
 
 // Datenbank starten
 database.initDB(function (err, db) {
-    if (db) {
-        UserService.createDefaultAdmin(function (err) {
-            if (err) {
-                console.log(err)
-            }
-        })
-        console.log("Connection to database successful.")
-        console.log("========================================================================================")
-    } else {
-        console.error("Connection to database failed.")
-    }
-})
+	if (db) {
+		UserService.createDefaultAdmin(function (err) {
+			if (err) {
+				console.log(err);
+			}
+		});
+		console.log('Connection to database successful.');
+		console.log('========================================================================================');
+	} else {
+		console.error('Connection to database failed.');
+	}
+});
 
 // Adding Routes
-app.use("/", testRoutes);
-app.use("/api/publicUsers", publicUserRoutes)
-app.use("/api/users", userRoutes)
-app.use("/api/degreeCourses", degreeCourseRoutes)
-app.use("/api/authenticate", authenticateRoute)
-app.use("/api/degreeCourseApplications", degreeCourseApplicationRoutes)
+app.use('/', testRoutes);
+app.use('/api/publicUsers', publicUserRoutes);
+app.use('/api/users', userRoutes);
+app.use('/api/degreeCourses', degreeCourseRoutes);
+app.use('/api/authenticate', authenticateRoute);
+app.use('/api/degreeCourseApplications', degreeCourseApplicationRoutes);
 
 /**
  * Serverseitige Error Handlers:
- * 
+ *
  *  1. Serverseitiger Fehler
  *  2. Userseitiger Fehler
  */
 app.use(function (err, req, res, next) {
-    console.error(err.stack)
-    res.status(500).send("Something went wrong on our side. :( Sorry!")
-})
+	console.error(err.stack);
+	res.status(500).send('Something went wrong on our side. :( Sorry!');
+});
 
 app.use(function (err, req, res, next) {
-    res.status(404).send("Sorry, we cannot find the URL. Please pass in a valid URL.")
-})
+	res.status(404).send('Sorry, we cannot find the URL. Please pass in a valid URL.');
+});
 
 const server = https.createServer({ key: key, cert: cert }, app);
 
 // app.get('/', (req, res) => { res.send('this is an secure server') })
 
-app.listen(80, () => {
-    console.log("Http app listening on port https://localhost/80");
-})
+// original port 80
+app.listen(1025, () => {
+	console.log('Http app listening on port http://localhost/1025');
+});
 
 // App Start
-const port = 443
+// original port 443
+const port = 1026;
 server.listen(port, () => {
-    console.log(`Example app listening on port https://localhost:${port}`)
-    console.log(new Date().toISOString())
-    console.log("========================================================================================")
-})
+	console.log(`Example app listening on port https://localhost:${port}`);
+	console.log(new Date().toISOString());
+	console.log('========================================================================================');
+});
 
 // const portHTTP = 80
 // app.listen(port, () => {
