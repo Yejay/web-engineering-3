3bf9e9c3162d963d4fc730e2d6ce650f70c5d016
Yejay <demirkan.yejay@gmail.com>
2023-07-04 16:35:17 +0200

    clean up

--- backend-kelvin/playground.js
+++
@@ -1,6 +0,0 @@
-let undefinedd = 2;
-
-
-let boo  = typeof undefinedd == "undefined"
-
-console.log(boo);
--- taro-unit-tests/UnauthorizedBomb.tsx
+++
@@ -1,5 +0,0 @@
-import { ErrorWithHTML } from "../../backend/boardapi";
-
-export default function UnauthorizedBomb(): JSX.Element {
-    throw new ErrorWithHTML(401, 'Unauthorized');
-}
